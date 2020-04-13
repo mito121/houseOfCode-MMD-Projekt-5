2cc6f379e52b5b24a622c8b9b9e9282c4996f798
Rasmus Andersen <40599932+mito121314@users.noreply.github.com>
2020-04-13 10:55:51 +0200

    sådan der

---
+++ pages/hvad-er-en-app.php
@@ -0,0 +1,36 @@
+<section class="bg-dark">
+
+    <div class="row">
+        <div class="code-content">
+            <h1 class="d-inline orange">Hvad er en app?</h1> {
+            <div class="indent">
+                <span class="yellow">R&aring;dgivning</span>, <span class="purple">design</span>, <span class="blue">udvikling</span>&nbsp;&amp;&nbsp;<span class="red">vedligeholdelse </span>af apps
+            </div>
+            }
+        </div>
+    </div>
+
+    &nbsp;
+
+    <div class="row">
+        <h2 class="red">Hvordan fungerer en app?</h2>
+        <p>Typisk kendes apps som små tilføjelsesprogrammer til eksempelvis smartphones. Mange kender apps som facebook til mobilen, spillet angry birds og funktionen lommelygte, der gør brug af mobilens flash. Det er altså små programmer eller software, som fungerer via en computer eller et API. <br><b>Læs mere...</b><br> Der findes et hav af apps inden for mange forskellige områder. Man kan få apps til at hjælpe med regnskab, bogføring, billedredigering, projektstyring, vejvisning og meget andet.</p>
+    </div>
+
+    &nbsp;
+
+    <div class="row">
+        <h2 class="blue">Hvad kan i opnå med en app?</h2>
+        <p>Mobilteknologien er i en rivende udvikling. Smartphone produkter som iPhone og Android har stor succes, og forbrugermarkedet driver derfor app-revolutionen. For virksomheder ligger der mange gemte muligheder i denne udvikling, men det kræver både vilje, mod og evner at ride med på bølgen. <br><b>Læs mere...</b><br> Over de sidste mange år har flere og flere mennesker dropper deres stationære og bærbare computere til fordel for tablets og smartphones. I begyndelsen af 2018 kom 51,2% af den globale internettrafik fra mobile enheder.</p>
+    </div>
+
+    &nbsp;
+
+    <div class="row">
+        <h2 class="green">Tilkobling til eksisterende systemer</h2>
+        <p>API'er gør det muligt for forskellige programmer at ”tale” sammen, og det kan blandt andet betyde, at du med API kan udveksle data på tværs af programmer. <br><b>Læs mere...</b><br> Fordelen ved et API er først og fremmest, at det letter arbejdet, fordi du slipper for manuelt at overføre oplysninger, som eksempelvis fra din webshop til dit regnskabsprogram.
+            Et godt API gør det ydermere lettere at udvikle et nyt program, fordi det kan forbinde og give adgang til alle oplysninger og ”byggeklodser”, der skal bruges. Når alle oplysninger er tilgængelige og samlet, kan programmøren lettere udvikle og få tingene til at arbejde sammen.</p>
+    </div>
+
+
+</section>
